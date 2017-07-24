7c32bd29cfd9ac95444ded4aae596efe6227d676
Rabin Rai <rabinrai44@gmail.com>
2017-07-24 13:13:31 -0400

    Completed get in touch with create custom jquery modal.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,6 +3,7 @@ import MobiMenu from './modules/MobiMenu';
 import RevealOnScroll from './modules/RevealonScroll';
 import $ from 'jquery';
 import StickyHeader from './modules/StickyHeader';
+import Modal from './modules/Modal';
 
 
 var mobileMenu        = new MobiMenu();
@@ -10,3 +11,4 @@ new RevealOnScroll($(".service"), "75%");
 new RevealOnScroll($(".reveal-item--wow"), "85%");
 new RevealOnScroll($(".p-item"), "70%");
 var stickyHeader = new StickyHeader();
+var modal = new Modal();
